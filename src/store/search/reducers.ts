62a627adfb0f115289748b81ea2b30f80a5483d6
joel linder <Jollescott.carl@hotmail.com>
2019-04-24 14:43:32 +0200

    Work on reducer

--- src/store/search/reducers.ts
+++ src/store/search/reducers.ts
@@ -1,11 +1,30 @@
 import { ISearchState, SearchActionTypes } from "./types";
 
 const initialState: ISearchState = {
+    error: '',
+    loading: false,
     results: [],
 }
 
 export function searchReducer(state = initialState, action: SearchActionTypes) : ISearchState {
     switch(action.type){
+        case 'SEARCH_START':
+            return {
+                ...state,
+                loading: true
+            };
+        case 'SEARCH_FAILURE':
+            return {
+                ...state,
+                loading: false,
+                error: action.error
+            };
+        case 'SEARCH_SUCCESS':
+            return {
+                ...state,
+                results: state.results,
+                loading: false
+            }
         default: 
             return state;
     }
--- src/store/search/types.ts
+++ src/store/search/types.ts
@@ -7,14 +7,29 @@ export interface IIngredient{
 }
 
 export interface ISearchState{
-    results: IRecipe[];
+    results: IRecipe[],
+    loading: boolean,
+    error: string
 }
 
 export const SEARCH_START = 'SEARCH_START';
 
 interface ISearchStartAction {
     type: typeof SEARCH_START;
-    ingredients: IIngredient[] 
 }
 
-export type SearchActionTypes = ISearchStartAction;
+export const SEARCH_SUCCESS = 'SEARCH_SUCCESS';
+
+interface ISearchSuccessAction {
+    type: typeof SEARCH_SUCCESS,
+    recipes: IRecipe[]
+}
+
+export const SEARCH_FAILURE = 'SEARCH_FAILURE';
+
+interface ISearchFailureAction {
+    type: typeof SEARCH_FAILURE,
+    error: string
+}
+
+export type SearchActionTypes = ISearchStartAction | ISearchSuccessAction | ISearchFailureAction;
