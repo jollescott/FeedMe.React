38a094b35db5ce02c5a532ffb524f6f4d7dcc735
joel linder <Jollescott.carl@hotmail.com>
2019-05-07 11:39:03 +0200

    stable version

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,51 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import './App.css';
-import logo from './logo.svg';
+import Slider from 'react-slick';
 import { rootReducer } from './store';
 
-const store = createStore( 
-  rootReducer,
-  applyMiddleware(thunk)
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const App: React.FC = () => {
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 1,
+    speed: 500,
+    dots: true
+};
+
   return (
     <Provider store={store}>
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h2>Center Mode</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
     </Provider>
   );
-}
+};
 
 export default App;
