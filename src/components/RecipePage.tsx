e002325e9f714c45cb3df2f0a89a20a1e4327135
Filip Johnsson <FiJo0302@student.grillska.se>
2019-05-20 23:00:04 +0200

    RecipePage nästan klar (behöver styling)

--- src/components/RecipePage.tsx
+++ src/components/RecipePage.tsx
@@ -6,7 +6,8 @@ import { connect } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from 'redux';
 import { goBack } from '../store/carousel/actions';
-import { Button } from '@material-ui/core';
+import { Button, Card, CardMedia, CardContent, Typography, Divider } from '@material-ui/core';
+import { isNumber } from 'util';
 
 interface IRecipeProps {
     recipe: IRecipe | undefined;
@@ -48,15 +49,82 @@ class RecipePage extends React.Component<IRecipeProps, IRecipeState>
 
     private renderRecipe() {
         if (this.props.recipe !== undefined) {
+            const recipe = this.props.recipe;
             return (
-                <h1>{this.props.recipe.name}</h1>
+                <div>
+                    <div>
+                        <img src={recipe.image} />
+                        <h1>{this.props.recipe.name}</h1>
+                    </div>
+                    <div className="doubleColumnContainer">
+                        <div className="doubleColumnColumn">
+                            <Card className="recipeInstructionCard">
+                                <CardContent className="cardText">
+                                    <Typography gutterBottom={true} variant="h6">
+                                        Ingredienser
+                                    </Typography>
+
+                                    <div className="recipeIngredientListLeft">
+                                        {recipe.recipeParts.map((recipePart, index) => (
+                                            <div>
+                                                <div className="recipeIngredientListRow">
+                                                    <div className="recipeIngredientListLeftColumn">
+                                                        <Typography component="p">
+                                                            {recipePart.quantity + recipePart.unit}
+                                                        </Typography>
+                                                    </div>
+                                                    <div className="recipeIngredientListRightColumn">
+                                                        <Typography component="p">
+                                                            {recipePart.ingredientName}
+                                                        </Typography>
+                                                    </div>
+                                                </div>
+                                                <Divider variant="fullWidth" />
+                                            </div>
+                                        ))}
+                                    </div>
+
+                                </CardContent>
+                            </Card>
+                        </div>
+                        <div className="doubleColumnColumn">
+                            {recipe.directions.map((direction, index) => (
+                                <Card className="recipeInstructionCard">
+                                    <CardContent className="cardText">
+                                        <Typography gutterBottom={true} variant="h6">
+                                            {"Steg " + (index + 1) + "."}
+                                        </Typography>
+                                        <Typography component="p">
+                                            {this.formatRecipeDirection(direction)}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             );
         }
         else {
             return (
-                <h1>Hejj</h1>
+                <h1>Kunde inte hämta receptet</h1>
             );
         }
+    }
+
+    // Funktion som ser till att en receptinstruktion ser bra ut
+    private formatRecipeDirection(direction: string): string {
+        direction = direction.trim();
+        // Tar bort receptinstruktionsnummer om det finns.
+        while (this.stringIsNumber(direction[0])) {
+            direction = direction.slice(1, direction.length - 1);
+        }
+        return direction;
+    }
+
+    // Returerar true om stringen är ett nummer
+    private stringIsNumber(value: string): boolean {
+        return ((value != null) && !isNaN(Number(value)));
     }
 }
 
